a56322359c89d85d5b3c01db392112136877a307
Harry Maynard <maynard321@gmail.com>
2024-08-14 21:56:55 -0700

    Reading files from action `include` param.

--- action.js
+++ action.js
@@ -1,12 +1,29 @@
 #! /usr/bin/env node
 
 import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { marked } from 'marked'
+import { glob } from 'glob'
 
-const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.')
+const include = process.env.INPUT_INCLUDE
+const includes = include.split('\\n')
+let allMarkdown = ''
+
+console.log('include:', include)
+
+for (const i in includes) {
+  const regex = includes[i]
+  const markdownFileNames = await glob(regex.trim(), { ignore: 'node_modules/**' })
+  console.log('markdownFileNames:', markdownFileNames)
+  for (const j in markdownFileNames) {
+    const markdownFileName = markdownFileNames[j]
+    const markdown = fs.readFileSync(path.resolve(import.meta.dirname, markdownFileName)).toString().trim()
+    allMarkdown += `${markdown}\n\n`
+  }
+}
+//console.log('allMarkdown:', allMarkdown)
+const html = marked.parse(allMarkdown)
 console.log('html:', html, '\n')
 fs.writeFileSync('example.html', html)
-
-console.log('process.argv:', process.argv)
-console.log('process.env:', process.env)
 
